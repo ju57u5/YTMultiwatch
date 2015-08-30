10b5cdd3eb04db33c39d86cf825aada2bccbd573
ju57u5 <geldtransporter@hotmail.de>
2015-08-30 21:10:48 +0200

    Submit videos with get

--- multiwatch.js
+++ multiwatch.js
@@ -1,29 +1,3 @@
-function powerVideos() {
-	videos = document.querySelectorAll("video");
-	for (var i = 0, l = videos.length; i < l; i++) {
-		var video = videos[i];
-		var src = video.src || (function () {
-			var sources = video.querySelectorAll("source");
-			for (var j = 0, sl = sources.length; j < sl; j++) {
-				var source = sources[j];
-				var type = source.type;
-				var isMp4 = type.indexOf("mp4") != -1;
-				if (isMp4) return source.src;
-			}
-			return null;
-		})();
-		if (src) {
-			var isYoutube = src && src.match(/(?:youtu|youtube)(?:\.com|\.be)\/([\w\W]+)/i);
-			if (isYoutube) {
-				var id = isYoutube[1].match(/watch\?v=|[\w\W]+/gi);
-				id = (id.length > 1) ? id.splice(1) : id;
-				id = id.toString();
-				var mp4url = "http://www.youtubeinmp4.com/redirect.php?video=";
-				video.src = mp4url + id;
-			}
-		}
-	}
-}
 function activate(id) {
 	$('video[id^=vid]').prop('muted', true);
 	$("#vid"+id).prop('muted', false);
@@ -37,20 +11,39 @@ function startAll() {
 	})
 }
 
+function getQueryParams(qs) {
+    qs = qs.split("+").join(" ");
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])]
+            = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
 $( document ).ready(function () {
-	var videos = [
+	var $_GET = getQueryParams(document.location.search);
+	console.log($_GET["vids"]); 
+	var videos = $_GET["vids"].split("|"); 
+	/*videos = [
 	'W7WRJRme8wg',
 	'P-Iho8dE5jE',
 	'di5wiSXoFew',
 	'FnfCSwLk0XY',
 	'zvwjGiPVoeQ',
 	'lJ5CWrAkseo'
-	];
+	W7WRJRme8wg|P-Iho8dE5jE|di5wiSXoFew|FnfCSwLk0XY|zvwjGiPVoeQ|lJ5CWrAkseo
+	];*/
 	var players = new Array();
 	$.each(videos, function (index, value) {
 		YoutubeVideo(value, function(video){
-			var vsrc =  video.getSource("video/mp4", "medium").url;
-			$('#main').append("<div class='hidden embed-responsive embed-responsive-16by9' id='parent"+index+"'><video src='"+ vsrc +"' id='vid"+index+"'></video></div>");	
+			var vsrc =  video.getSource("video/mp4", "hd720");
+			console.log(vsrc);
+			$('#main').append("<div class='hidden embed-responsive embed-responsive-16by9' id='parent"+index+"'><video src='"+ vsrc.url +"' id='vid"+index+"'></video></div>");	
 		});
 		
 	});
